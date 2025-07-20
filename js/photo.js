4884f28ccc6af75742d008b0f8283e144e8392ca
BagelDogM <bageldogm@gmail.com>
2025-07-20 02:45:22 +0100

    feat(photo): add more info to photo popups

--- js/photo.js
+++ js/photo.js
@@ -6,11 +6,12 @@ export {initPopupListeners, populatePhotos}
 
 function initPopupListeners() {
     const popup = document.querySelector('.image-popup');
+    const body = popup.querySelector(".popup-container")
     const img = document.querySelector('.popup-img');
     const text = document.querySelector('.popup-text');
 
     popup.addEventListener('click', (e) => {
-        if (e.target !== img && e.target !== text) {
+        if (!body.contains(e.target)) {
             popup.style.display = 'none';
         }
     })
@@ -22,7 +23,7 @@ function initPopupListeners() {
     })
 }
 
-function addPhotoListeners(photoContainer) {
+function addPhotoListeners(photoContainer, data) {
     photoContainer.addEventListener('click', () => {
         const imageSrc = photoContainer.querySelector('img').getAttribute('src');
 
@@ -34,9 +35,18 @@ function addPhotoListeners(photoContainer) {
         const popup = document.querySelector('.image-popup');
         popup.style.display = '';
 
-        // Set the link
-        const link = document.querySelector('.popup-link');
-        link.href = imageSrc;
+        // Set the link & general content
+        document.querySelector('.plink').href = imageSrc;
+        document.querySelector(".pheading").textContent = data.heading || "No title";
+        document.querySelector(".psubheading").textContent = data.subheading || ""
+        document.querySelector(".pdesc").textContent = data.desc || "";
+
+        // Set metadata
+        const date = new Date(data.date * 1000) // multiply unix timestamp by 1000 to get milliseconds
+        const formattedDate = date.toLocaleDateString("en-ca") // format as YYYY-MM-DD
+        document.querySelector(".date-value").textContent = formattedDate
+
+        document.querySelector(".camera-value").textContent = data.camera
     });
 }
 
@@ -83,7 +93,7 @@ function populatePhotos(count) {
                     text.appendChild(shead)
                 }
 
-                addPhotoListeners(photoEntry);
+                addPhotoListeners(photoEntry, photo);
 
                 gallery.appendChild(photoEntry);
             }
